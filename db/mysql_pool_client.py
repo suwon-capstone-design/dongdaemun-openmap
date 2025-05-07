09de5f7c67e71898ec9e28cce964e414896d46d3
HOYA <als77770@gmail.com>
2025-05-07 17:27:01 +0900

    Revert "fix: sys path 추가"
    This reverts commit 3d8f7e9c0eba2d88fd7283de6aaad5344ae4cb3b.

--- db/mysql_pool_client.py
+++ db/mysql_pool_client.py
@@ -1,6 +1,4 @@
-import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from sshtunnel import SSHTunnelForwarder
 from dbutils.pooled_db import PooledDB
 import pymysql
