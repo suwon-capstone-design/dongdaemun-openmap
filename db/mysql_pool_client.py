3d8f7e9c0eba2d88fd7283de6aaad5344ae4cb3b
HOYA <als77770@gmail.com>
2025-05-07 17:24:49 +0900

    fix: sys path 추가

--- db/mysql_pool_client.py
+++ db/mysql_pool_client.py
@@ -1,4 +1,6 @@
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from sshtunnel import SSHTunnelForwarder
 from dbutils.pooled_db import PooledDB
 import pymysql
